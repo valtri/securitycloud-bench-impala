4714efd5b5683e61978c83206155f120d23ad1c3
František Dvořák <valtri@civ.zcu.cz>
2015-12-15 21:00:12 +0100

    Update HOWTO.

--- HOWTO.sh
+++ HOWTO.sh
@@ -1,8 +1,18 @@
-#! /bin/sh
+#! /bin/sh -xe
 
-# once:
-../benchmarking/run.pl 2>&1 | tee log
+#
+# 1) get benchmark framework (as parent directory, not a subdirectory)
+#
+#   git clone https://github.com/valtri/benchmarking ../benchmarking
+#
+# 2) create proper settings.sh file
+#
 
-# repeat:
-for i in `seq 1 5`; do rm -rf result${i}; done
-for i in `seq 1 5`; do ../benchmarking/run.pl -o result${i} 2>&1 | tee ${i}.log; done
+# custom parameters
+#rm -rf results/; N=2 FILE="netflow_csv_anon_1" ../benchmarking/run.pl -s impala-text
+
+for f in anon_1 anon anon_big; do
+  rm -rf results/${f}.log
+  FILE="netflow_csv_${f}" ../benchmarking/run.pl -o result.${f} 2>&1 | tee ${f}.log
+  PREFIX="${f}-" ../benchmarking/averager.pl result.${f}
+done
