53c5339b7463307fc29907d731f2910ace886070
František Dvořák <valtri@civ.zcu.cz>
2016-04-06 22:46:58 +0200

    Update.

--- bench-queries.sh
+++ bench-queries.sh
@@ -7,12 +7,14 @@
 #
 # 2) create proper settings.sh file
 #
+# 3) store the sample file into $SRCFILE
+#
 
 # custom parameters
 #rm -rf results/; N=2 FILE="netflow_csv_anon_1" ../benchmarking/run.pl -s impala-text
 
 for f in anon_1 anon anon_big; do
   rm -rf results/${f}.log
-  FILE="netflow_csv_${f}" ../benchmarking/run.pl -o result.${f} 2>&1 | tee ${f}.log
-  PREFIX="${f}" ../benchmarking/averager.pl result.${f}
+  FILE="netflow_csv_${f}" ../benchmarking/run.pl -o result-query.${f} -s hive,impala-text,impala-parquet 2>&1 | tee ${f}.log
+  PREFIX="query-${f}" ../benchmarking/averager.pl result-query.${f}
 done
